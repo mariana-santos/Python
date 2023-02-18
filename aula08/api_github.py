ca5bf884cbd57e33e6f90fac4fa4959194754edf
mariana-santos <marianasfernandessousa@gmail.com>
2023-02-17 23:03:33 -0300

    exercicio api verificação disponibilidade de domínios

--- aula08/api_github.py
+++ aula08/api_github.py
@@ -1,15 +1,15 @@
 from urllib import response
-import requests
+import pip._vendor.requests 
 
 user = input('Digite o user do github que deseja pesquisar: ')
 
 url = f"https://api.github.com/users/{user}"
-response = requests.get(url)
+response = pip._vendor.requests.get(url)
 
 while (response.status_code != 200):
     user = input('Usuário inválido. Por favor pesquise outro: ')
     url = f"https://api.github.com/users/{user}"
-    response = requests.get(url)
+    response = pip._vendor.request.get(url)
 
 dados = response.json()
 print(f"Nome: {dados['name']}")
